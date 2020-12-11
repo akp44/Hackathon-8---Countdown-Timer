cb7758c21f41cb2afbda4e81e6b3878bf52f2da3
akshaypandey44 <kumarakshaypandey@gmail.com>
2020-12-11 16:24:58 +0530

    checking

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,48 @@
 import React, { Component, useState, useEffect } from "react";
 import '../styles/App.css';
 
-const App = () => {
-  // write your code here 
+export function Fun(props){ 
+  const [currentCount,setCount] = useState(Math.floor(props.value));
+  const timer = () => setCount(currentCount-1);
+  useEffect(()=>{
+    if(currentCount <= 0){
+      return;
+    }
+    const id = setInterval(timer,1000);
+    return () => clearInterval(id);
+  },[currentCount]);
+  return(
+    <>
+     {isNaN(Number(props.value)) ?  0 : <div>{currentCount}</div>}
+   </>
+  )   
+}
 
+const App = () => {
+  
+  const [newItem,setNewItem] = useState();
+  const [trigger,setTrigger] = useState(0);
+  const newItemChanged = (evt) =>{
+		setNewItem(evt.target.value);
+  }
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input
+          id="timeCount" 
+          onKeyDown={event => {
+            if(event.keyCode == 13){
+              setTrigger(trigger+1);
+              };
+            }
+          }
+          value={newItem}
+          onChange={newItemChanged}
+           /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{trigger != 0 ? <h1><Fun key={trigger} value={newItem}/></h1> : null}</div>
     </div>
   )
 }
